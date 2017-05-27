711bbf9dca2b888b98744e59f7e6129e3642e219
gaearrow <gaearrow@gmail.com>
2017-05-27 16:56:51 +0800

    Update 03mssqlchecker.py
    add input filter
    add info stats
    add checking prompt
    add try&except

--- 03mssqlchecker.py
+++ 03mssqlchecker.py
@@ -16,41 +16,70 @@ srcfile = sys.argv[1].strip()
 fsrc = open(srcfile,'r')
 fsql = open('sqlchecklist.txt','w')
 
+# for stats.
+nsqlser = 0
+ncmdenable = 0
+ncmddisable = 0
+
+# check one by one
 for eachline in fsrc:
+
+    # check input format.
+    if eachline.count(';') != 3:
+       print >>fsql,eachline
+       continue
+    nsqlser = nsqlser + 1
     strlist = eachline.strip().split(';')
     print >>fsql,strlist[0]+';',
+    print 'Checking '+strlist[0]
+
+    # connect sql server
     try:
        conn = pymssql.connect(host=strlist[0],user=strlist[2],password=strlist[3],database='master',port=strlist[1],login_timeout=30,charset='utf8')
        cursor = conn.cursor()
+       print >>fsql,'online;',
     except Exception as e:
         #print 'Error: %s' % e
         print >>fsql,'offline;',
         continue
 
-    print >>fsql,'online;',
-
     # get xp_cmdshell status
-    cursor.execute("exec sp_configure 'show advanced option'")
-    advopt = cursor.fetchall()
-    if advopt[0][3] == 0:
-        print >>fsql,'Disabled'+';',
-    else:
+    try:
         cursor.execute("exec sp_configure 'xp_cmdshell'")
         xpcmd = cursor.fetchall()
         if xpcmd[0][3] == 1:
             print >>fsql,'Enabled'+';',
+            ncmdenable = ncmdenable + 1
         else:
             print >>fsql,'Disabled'+';',
+            ncmddisable = ncmddisable + 1
+    except Exception as e:
+        #print 'Error: %s' % e
+        print >>fsql,'Disabled'+';',
+        ncmddisable = ncmddisable + 1
+
     # get sql version
-    cursor.execute("select @@version;")
-    sqlinfo = cursor.fetchall()
-    sqlverno = sqlinfo[0][0].split('\n\t')[0]
-    sqlvno = sqlverno.split('-')[1]
-    sqlver = sqlverno.split('-')[0]
-    osver = sqlinfo[0][0].split('on')[-1].strip()
-    print >>fsql,sqlver+';'+sqlvno+';'+osver
-        
+    try:
+        cursor.execute("select @@version;")
+        sqlinfo = cursor.fetchall()
+        sqlverno = sqlinfo[0][0].split('\n\t')[0]
+        sqlvno = sqlverno.split('-')[1]
+        sqlver = sqlverno.split('-')[0]
+        osver = sqlinfo[0][0].split('on')[-1].strip()
+        print >>fsql,sqlver+';'+sqlvno+';'+osver
+    except Exception as e:
+        #print 'Error: %s' % e
+        conn.close()
+        continue
+       
     conn.close()
+
+print '==================================='
+print 'MSSQL Check Information @Gaearrow'
+print 'Total Target : ', nsqlser
+print 'Cmdshell Enabled  : ', ncmdenable
+print 'Cmdshell Disabled : ', ncmddisable
+print '==================================='
 
 fsrc.close()
 fsql.close()
